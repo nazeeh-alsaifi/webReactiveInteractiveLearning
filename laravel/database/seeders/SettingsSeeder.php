a67b493c9eb9891954e437142feb704b4bd0bcfe
nazeeh-alsaifi <nazeeh.saifi@gmail.com>
2020-11-10 19:02:00 +0200

    seeding subject & nationality  & category & subcategory

--- laravel/database/seeders/SettingsSeeder.php
+++ laravel/database/seeders/SettingsSeeder.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Seeder;
 use App\Models\settings\AcademicLevel;
 use App\Models\settings\Country;
 use App\Models\settings\City;
+use App\Models\settings\Subject;
+use App\Models\settings\Nationality;
+use App\Models\settings\Category;
+use App\Models\settings\SubCategory;
 
 
 class SettingsSeeder extends Seeder
@@ -56,10 +60,101 @@ class SettingsSeeder extends Seeder
         ]);
         City::create([
             // "id"=>2,
+            "Country_id" => 1,
+            "city_name" => "Aleppo",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        City::create([
+            // "id"=>3,
             "Country_id" => 2,
             "city_name" => "Cairo",
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+        City::create([
+            // "id"=>4,
+            "Country_id" => 2,
+            "city_name" => "Jiza",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // Subject
+        Subject::create([
+            "Subject_name" =>"Math",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Subject::create([
+            "Subject_name" =>"Physics",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // Nationalities
+        Nationality::create([
+            "Nationality_name" => "German",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Nationality::create([
+            "Nationality_name" => "Syrian",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+       
+        // Categories
+        Category::create([
+            "subject_id" => 1,
+            "Cat_name" => "Category 1 of subject 1",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Category::create([
+            "subject_id" => 1,
+            "Cat_name" => "Category 2 of subject 1",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Category::create([
+            "subject_id" => 2,
+            "Cat_name" => "Category 1 of subject 2",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Category::create([
+            "subject_id" => 2,
+            "Cat_name" => "Category 2 of subject 2",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+       
+        // Sub Categories
+        SubCategory::create([
+            "category_id" => 1,
+            "sub_cat_name" => "SubCategory 1 of catergory 1",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        SubCategory::create([
+            "category_id" => 1,
+            "sub_cat_name" => "SubCategory 2 of category 1",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        SubCategory::create([
+            "category_id" => 2,
+            "sub_cat_name" => "SubCategory 1 of category 2",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        SubCategory::create([
+            "category_id" => 2,
+            "sub_cat_name" => "SubCategory 2 of category 2",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
+
 }
