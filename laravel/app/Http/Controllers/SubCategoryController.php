f3fd6b580fa11f71d4be9b2ef35fb574ebc781d5
Sleman2020 <slemanarmooshsss@gmailcom>
2020-11-12 07:49:39 +0200

    subCategory dashboard

---
+++ laravel/app/Http/Controllers/SubCategoryController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\settings\Category;
+use App\Models\settings\SubCategory;
+
+class SubCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return SubCategory::all();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getpage()
+    {
+        $sortField = request('sort_field','id');
+        if(!in_array($sortField,['id','category_id','sub_cat_name'])){
+            $sortField = 'id';
+        }
+        $sortDirection = request('sort_direction','desc');
+        if(!in_array($sortDirection,['asc','dec'])){
+            $sortDirection = 'desc';
+        }
+        $sub_categories = SubCategory::when(request('search','') != '', function($query){
+            $query->where('sub_cat_name','LIKE','%'.request('search').'%');
+        })->orderBy($sortField,$sortDirection)->paginate(5);
+        return $sub_categories;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'Id_cat' => 'required',
+             'sub_ct_name' => 'required'
+      ]);
+          $sub_category = new SubCategory;
+          $sub_category->category_id = $request->input('Id_cat');
+          $sub_category->sub_cat_name = $request->input('sub_ct_name');
+          $sub_category->save();
+          return  $sub_category;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store1(Request $request)
+    {
+        $this->validate($request, [
+            'category_id' => 'required',
+             'sub_cat_name' => 'required'
+      ]);
+          $id = $request->input('id');
+          $sub_category = SubCategory::find($id);
+          $sub_category->category_id = $request->input('category_id');
+          $sub_category->sub_cat_name = $request->input('sub_cat_name');
+          $sub_category->save();
+          return  $sub_category;
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $sub_category = SubCategory::find($id);
+        $sub_category->delete();
+        return true;
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -30,6 +30,7 @@ Route::group(['middleware' => 'api'], function ($router) {
     Route::resource('Component','ComponentController');
     Route::resource('Measuretool','MeasureToolsController');
     Route::resource('Nationality','NationalityController');
+    Route::resource('SubCategory','SubCategoryController');
     Route::post('Purchase/Free_Trail','PurchaseController@Free_Trail');
     Route::post('Purchase/Free_Student','PurchaseController@Free_Student');
     ////
@@ -40,6 +41,7 @@ Route::group(['middleware' => 'api'], function ($router) {
     Route::get('component/getpage','ComponentController@getpage');
     Route::get('measuretool/getpage','MeasureToolsController@getpage');
     Route::get('nationality/getpage','NationalityController@getpage');
+    Route::get('subCategory/getpage','SubCategoryController@getpage');
     //
     Route::post('academiclevels/store1','AcademiclevelController@store1');
     Route::post('countries/store1','CountryController@store1');
@@ -48,6 +50,7 @@ Route::group(['middleware' => 'api'], function ($router) {
     Route::post('components/store1','ComponentController@store1');
     Route::post('measuretools/store1','MeasureToolsController@store1');
     Route::post('nationalities/store1','NationalityController@store1');
+    Route::post('subCategories/store1','SubCategoryController@store1');
     //
     Route::resource('notes', 'NotesController');
 
