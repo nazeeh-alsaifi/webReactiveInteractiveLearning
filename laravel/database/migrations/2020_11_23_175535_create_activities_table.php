5bd9e6b359337bb4aa2c0fa60073b12784716a95
nazeeh-alsaifi <nazeeh.saifi@gmail.com>
2020-11-24 11:03:11 +0200

    make migrations for : 1- activities 2- tags 3- activities_tags 4- activities_institution_classes 5- activities_sections

---
+++ laravel/database/migrations/2020_11_23_175535_create_activities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->mediumText("objectives");
+            $table->string("image");
+            $table->unsignedBigInteger("subject_id");
+            $table->unsignedBigInteger("user_id");
+            $table->boolean("id_free");
+            $table->boolean("is_active");
+            $table->timestamps();
+            $table->foreign("subject_id")->references("id")->on("subjects");
+            $table->foreign("user_id")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
