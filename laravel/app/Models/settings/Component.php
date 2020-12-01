af62a0663509efb5a2d35c1f5a17da73bd6dc2e7
nazeeh-alsaifi <nazeeh.saifi@gmail.com>
2020-12-01 22:57:11 +0200

    created relationships between sections and components added data to the seeder to test these relationships

--- laravel/app/Models/settings/Component.php
+++ laravel/app/Models/settings/Component.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Component extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function sections(){
+        return $this->belongsToMany("sections_components","component_id","section_id");
+    }
+
+
 }
