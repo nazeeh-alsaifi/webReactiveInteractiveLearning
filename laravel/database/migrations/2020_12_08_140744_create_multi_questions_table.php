af1b0fb1554880c71f844e4e936dc3257818c22f
Sleman2020 <slemanarmooshsss@gmailcom>
2020-12-09 16:31:59 +0200

    build questions database with relations and seeding

---
+++ laravel/database/migrations/2020_12_08_140744_create_multi_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMultiQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('multi_questions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("sections_components_id");
+            $table->string('text_description')->nullable();
+            $table->string('Question');
+            $table->string("img_src")->default("no-image.jpg");
+            $table->timestamps();
+            $table->foreign("sections_components_id")->references("id")->on("sections_components");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('multi_questions');
+    }
+}
